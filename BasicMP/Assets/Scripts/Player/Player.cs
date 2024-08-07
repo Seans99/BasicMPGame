39523c3209bd8a4eaa6260bd24596609919de661
Sean Schelvis <sean.schelvis@hotmail.com>
2024-08-07 11:28:51 +0200

    Added movement

--- BasicMP/Assets/Scripts/Player/Player.cs
+++ BasicMP/Assets/Scripts/Player/Player.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float _moveSpeed = 5f;
+    public float _rotationSpeed = 50f;
+
+    private Vector2 _moveInput;
+    private Rigidbody2D _rb;
+
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        _rb.MovePosition(_rb.position + _moveInput * _moveSpeed * Time.fixedDeltaTime);
+
+        // Rotate player
+        if (_moveInput != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(_moveInput.y, _moveInput.x) * Mathf.Rad2Deg - 90f;
+
+            float targetRotation = Mathf.LerpAngle(_rb.rotation, angle, _rotationSpeed * Time.fixedDeltaTime);
+            _rb.rotation = targetRotation;
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+        _moveInput = value.Get<Vector2>();
     }
 }
